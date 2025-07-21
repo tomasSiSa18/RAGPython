aabc3d226516b649d11d6027e7a247ddaba02555
Tomas Sierra <t.sierras@uniandes.edu.co>
2025-07-21 13:36:01 -0500

    Cambios

--- RAGRetrieve.py
+++ RAGRetrieve.py
@@ -3,6 +3,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 from langchain.embeddings import OpenAIEmbeddings
 import psycopg2
+from tabulate import tabulate
 
 load_dotenv()
 
@@ -26,20 +27,32 @@ while prompt != "0":
     query_embedding = embedding_model.embed_query(prompt)
     cur = conn.cursor()
 
-    cur.execute(
-        """
-        SELECT content, embedding <-> %s::vector AS distance
-        FROM documents
-        ORDER BY distance
-        LIMIT 5;
-        """,
-        (query_embedding,)
-    )
+    distancias = ["<->", "<#>", "<=>"]
+    tipoDist = ["Euclideana", "Coseno", "Producto Interno"]
 
-    results = cur.fetchall()
-    for row in results:
-        content, distance = row
-        print(f"[{distance:.4f}] {content[:200]}...\n")
+    for dist, tipo in zip(distancias, tipoDist):
+
+        cur.execute(
+            f"""
+            SELECT origin, content, embedding {dist} %s::vector AS distance
+            FROM biomateriales
+            ORDER BY distance
+            LIMIT 5;
+            """,
+            (query_embedding,)
+        )
+
+        results = cur.fetchall()
+        finalList = []
+        headers = ["Origin", "Content", "Distance"]
+        for row in results:
+            origin, content, distance = row
+            rowTb = [origin, content[:200], round(distance, 4)]
+            finalList.append(rowTb)
+        
+        print(f"El resultado para la distancia {tipo} son:")
+        print(tabulate(finalList, headers=headers, tablefmt="grid"))
+    prompt = input("Ingrese un prompt:")
 
 
 cur.close()
